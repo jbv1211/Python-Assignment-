1005faefc3623ffbfdeb43e4e260a0edb2f213ca
jbv1211 <95849889+jbv1211@users.noreply.github.com>
2022-03-16 21:44:48 +0530

    Add files via upload

---
+++ stack.py
@@ -0,0 +1,26 @@
+# ID-->20CE008
+# Name-->Jay Bhimani
+# GitHub Link--> https://github.com/jbv1211/Python-Assignment-.git
+
+
+#-----------------------------QUESTION-----------------------------------------
+# WRITE A PROGRAM IN PYTHON TO IMPLEMENT A STACK DATA STRUCTURE USING CLASS AND OBJECTS, WITH PUSH, POP, AND TRAVERSAL METHOD.
+
+stack=[]
+
+stack.append('A')
+stack.append('B')
+stack.append('C')
+stack.append('D')
+stack.append('H')
+
+print('Ininial Stack')
+print(stack)
+
+print('\nElement popped from stack:')
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+
+print('\nStack After Element Are Popped:')
+print(stack)
