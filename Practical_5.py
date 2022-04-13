ffd45f2b5b117dff12a2fae831aaad54c6e1bc51
jbv1211 <95849889+jbv1211@users.noreply.github.com>
2022-04-13 10:21:11 +0530

    Swap Case

---
+++ Practical_5.py
@@ -0,0 +1,24 @@
+# ID-->20CE008
+# Name-->Jay Bhimani
+# GitHub Link--> https://github.com/jbv1211/Python-Assignment-.git
+
+'''
+You are given a string and your task is to swap cases. In other words, convert
+all lowercase letters to uppercase letters and vice versa.
+Sample Input: HackerRank.com presents "Pythonist 2".
+Sample Output: hACKERrANK.COM PRESENTS "pYTHONIST 2"
+ '''
+
+
+# Method 1
+# Using Function
+str=input("Enter the string ")
+print(str.swapcase())
+
+# Method 2
+# def swap_case(s):
+#     return s.swapcase()
+# if __name__ == '__main__':
+#     s = input()
+#     result = swap_case(s)
+#     print(result)
